777ebf205894c83b7f90fd9818b238acac2e2c8a
Andrew Goessling <agoessling@gmail.com>
2024-07-23 20:23:00 -0700

    Initial C formatting.

---
+++ hello_world.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+// This is a very long comment that needs to be wrapped lest you get no presents for christmas.  Jesus!
+int main(void) {
+  printf("Hello World!\n");
+  return 0;
+}
