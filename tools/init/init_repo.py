08e2dd917a052c9fe46b46df04f5d400fcbec744
Andrew Goessling <agoessling@gmail.com>
2024-07-25 20:53:26 -0700

    No bazel based copy.

--- tools/init/init_repo.py
+++ tools/init/init_repo.py
@@ -18,26 +18,25 @@ def get_root_from_this(this: str) -> pathlib.Path:
     return pathlib.Path(path).parents[2]
 
 
-def get_init_file_paths(rel_paths: list[str]) -> list[pathlib.Path]:
-    output = []
-
-    r = runfiles.Create()
-    for rel_path in rel_paths:
-        abs_path = r.Rlocation(rel_path)
-        if not abs_path:
-            raise FileNotFoundError(rel_path)
-        output.append(pathlib.Path(abs_path))
-
-    return output
-
-
-def copy_init_files(files: list[pathlib.Path], root: pathlib.Path) -> None:
+def copy_init_files(root: pathlib.Path) -> None:
     workspace_path = get_workspace()
-    for file in files:
-        rel_path = file.relative_to(root)
+
+    srcs = [
+        '.aspect',
+        'tools/format',
+        'tools/lint',
+        '.bazeliskrc',
+        '.clang-format',
+        '.clang-tidy',
+    ]
+    for rel_path in srcs:
+        src = root / pathlib.Path(rel_path)
         dest = workspace_path / rel_path
         dest.parent.mkdir(parents=True, exist_ok=True)
-        shutil.copyfile(file, dest)
+        if src.is_dir():
+            shutil.copytree(src, dest, dirs_exist_ok=True)
+        else:
+            shutil.copy(src, dest)
         print(f'Copied {rel_path}')
 
 
@@ -45,12 +44,13 @@ def main() -> None:
     parser = argparse.ArgumentParser(
         description='Copy linting and formatting config to current repo.')
 
-    parser.add_argument("--init_files", nargs='+', required=True, help='Files to copy to repo.')
-    parser.add_argument("--this", required=True, help='The $(rlocation) of this file.')
+    parser.add_argument("--this",
+                        required=True,
+                        help='The $(rlocation) of this file.')
 
     args = parser.parse_args()
 
-    copy_init_files(get_init_file_paths(args.init_files), get_root_from_this(args.this))
+    copy_init_files(get_root_from_this(args.this))
 
 
 if __name__ == '__main__':
